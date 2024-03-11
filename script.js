8e5fe594354d38d66768d413b94039fac4a04dd3
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-11 22:08:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	if (!str || !str.includes(' ')) {
+        return str; 
+    }
+	const firstSpaceIndex = str.indexOf(' ');
+    const firstWord = str.substring(0, firstSpaceIndex);
+
+    return firstWord;
 }
 
 // Do not change the code below
